17a6d9708d0da13c25408d85330e595a58450d7f
rumenkrumov3 <rumenkrumov3@gmail.com>
2021-09-13 16:56:51 +0300

    Starting
    Starting

---
+++ AdvanceModule/StacksAndQueuesLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueuesLab
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Lets start!");  
+        }
+    }
+}
